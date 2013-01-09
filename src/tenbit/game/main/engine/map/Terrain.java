0c7b4c4182cd2286181a1f27a3fb7d90dfeee318
Ryan Bell <Llaver@live.com>
2013-01-09 11:10:09 -0600

    Started Terrain Selection

--- src/tenbit/game/main/engine/map/Terrain.java
+++ src/tenbit/game/main/engine/map/Terrain.java
@@ -1,12 +1,31 @@
 package tenbit.game.main.engine.map;
 
 public class Terrain {
-
+	
+	private int terrainType;
+	private boolean isRandom = false;
+	
 	public Terrain() {
+		terrainType = -1;
 		
 	}
 	public void setTerrain(int choice) {
 		
 	}
-
+	public void setTerrain(boolean random) {
+		
+	}
+	private int getTerrain() {
+		return terrainType;
+		
+	}
+	private void checkType() {
+		if(terrainType < 0) 
+		{
+			isRandom = true;
+		}
+		else {
+			isRandom = false;
+		}
+	}
 }
