4ca75daacf4b0436be9104b172f9000344a025fc
Henry Chau <henrychau123@gmail.com>
2013-01-29 11:10:20 -0600

    Real Logging
    not fake (legit)

--- src/tenbit/game/main/system/logging/SystemLogger.java
+++ src/tenbit/game/main/system/logging/SystemLogger.java
@@ -1,19 +1,24 @@
 package tenbit.game.main.system.logging;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.lang.System;
 
+import tenbit.game.main.system.DateAndTime;
+
 public class SystemLogger {
+	private static String osVer = System.getProperty("os.name");;
+	private static String userHome = System.getProperty("user.home");;
 	
 	public SystemLogger() {
 		
 	}
 	
 	public static void createNewDir() {
-		try {
-			String osVer = System.getProperty("os.name");
-			String userHome = System.getProperty("user.home");
-			
+		try {	
 			if(osVer.contains("Windows XP")) {
 				userHome += "\\My Documents\\";
 			}
@@ -23,24 +28,50 @@ public class SystemLogger {
 			else {
 				System.out.println("Uh-oh :(");
 			}
-			System.out.println(userHome + "Reduc/");
-			//new File(userHome + "").mkdirs();
+			System.out.println(new File(userHome + "Reduc/logs/systemlogs/").mkdirs());
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void createNewLog() {
-		//File dir = new File("");
-		//http://stackoverflow.com/questions/6656565/create-a-text-file-in-a-folder
+	public static void createNewLog() {
+		try {
+			String logName = "syslog " + DateAndTime.getDate();
+			System.out.println(new File(userHome + "Reduc/logs/systemlogs/", logName + ".txt").createNewFile());
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public static void logAction() {
-		
+	public static void logAction(String s) {	
+		try {
+			BufferedReader b = new BufferedReader(new FileReader(userHome + "Reduc/logs/systemlogs/" + "syslog " + DateAndTime.getDate() + ".txt"));
+			
+			if(!(new File(userHome + "Reduc/logs/systemlogs/").exists())) {
+				createNewDir();
+			}
+			if(!(new File(userHome + "Reduc/logs/systemlogs/" + "syslog " + DateAndTime.getDate() + ".txt").exists())) {
+				createNewLog();
+			}
+			
+			File f = new File(userHome + "Reduc/logs/systemlogs/" + "syslog " + DateAndTime.getDate() + ".txt");
+			FileWriter fw = new FileWriter(f);
+			PrintWriter pw = new PrintWriter(fw);
+			
+			pw.println("[" + DateAndTime.getTime() + "]" + s);
+			pw.close();
+			pw.flush();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
-		SystemLogger.createNewDir();
+		SystemLogger.logAction("hi");
+		SystemLogger.logAction("hi2");
+		SystemLogger.logAction("h3i");
 	}
 }
